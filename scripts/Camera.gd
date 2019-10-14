5785f24c71613144a0ec507994dc99c8d51978be
Angelo Aracri <angeloaracri@yahoo.de>
2019-10-14 17:29:09 +0200

    Added scrolling, fixed project display settings, fixed background

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -6,6 +6,14 @@ var drag_offset = Vector2()
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
+			if event.button_index == BUTTON_WHEEL_UP:
+				zoom.x = min(2, zoom.x * 1.02)
+				zoom.y = min(2, zoom.y * 1.02)
+				return
+			if event.button_index == BUTTON_WHEEL_DOWN:
+				zoom.x = max(1, zoom.x / 1.02)
+				zoom.y = max(1, zoom.y / 1.02)
+				return
 			dragging = true
 			drag_offset = event.position + offset
 		else:
